05c59b53b404aee433bbafede5c5ef8bd4f0da93
jmlengineering <jasmineleeengineering@gmail.com>
2022-06-10 21:59:09 -0400

    add Component state and event handler

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@
 // import './App.css';
 import { useState } from "react"
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter( counter + 1 )
+  const decreaseByOne = () => setCounter( counter - 1 )
   const setToZero = () => setCounter(0)
- 
+
   console.log('rendering...', counter)
 
   return (
-   <div>
-    <div>{ counter }</div>
-    <button onClick={increaseByOne}>
+  <div>
+    <Display counter = { counter }/>
+    <button onClick={ increaseByOne }>
       plus
     </button>
-    <button onClick={setToZero}>
+    <button onClick={ setToZero }>
       zero
     </button>
-   </div>
+    <button onClick={ decreaseByOne }>
+      minus
+    </button>
+  </div>
   ) 
 } 
 
